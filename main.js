30b4597dc5a9637fa2d62a4a67ee939272224f9e
landesprit <julien.bataille40@gmail.com>
2024-01-29 11:22:39 +0100

    MAJ

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
+let alarmAudio = new Audio("/sounds/alarm.wav");
+let settingsHour = parseInt(document.getElementById("settingsHour").textContent);
+let settingsMinute = parseInt(document.getElementById("settingsMinute").textContent);
+
 function updateClock(){
+
     let now = new Date();
     let milli = now.getMilliseconds(),
         sec = now.getSeconds(),
@@ -12,9 +17,13 @@ function updateClock(){
     let tags = ["mon","d","y","h","m","s","mi"],
         corr = [month[mo],dy,yr,hou,min,sec,milli];
 
-    for(let i = 0; i < tags.length; i++ )
+    for(let i = 0; i < tags.length; i++ ){
         document.getElementById(tags[i]).firstChild.nodeValue = corr[i] ;
-    
+    }
+
+    if (hou == settingsHour && min == settingsMinute){
+        playSound(alarmAudio)
+     }
 
 }; 
 
@@ -22,3 +31,8 @@ function initClock (){
     updateClock();
     window.setInterval("updateClock()", 1);
 };
+
+function playSound(sound) {
+    sound.play();
+}
+
